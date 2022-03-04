4770a2c606c9c629dabf246718a8e115846dfd99
Dmitriy Aratin <dima.aratin0@gmail.com>
2022-03-04 11:50:09 +0700

    f

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 alfavit = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M',
            13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y',
-           25: 'Z', 26: 'A', 27: 'B', 28: 'C', 29: 'D', 30: 'E', 31: 'F', 32: 'G', 33: 'H', 34: 'I', 35: 'J', 36: 'K',
-           37: 'L', 38: 'M', 39: 'N', 40: 'O', 41: 'P', 42: 'Q', 43: 'R', 44: 'S', 45: 'T', 46: 'U', 47: 'V', 48: 'W',
-           49: 'X', 50: 'Y', 51: 'Z', 52: 'А', 53: 'Б', 54: 'В', 55: 'Г', 56: 'Д', 57: 'Е', 58: 'Ё', 59: 'Ж', 60: 'З',
-           61: 'И', 62: 'Й', 63: 'К', 64: 'Л', 65: 'М', 66: 'Н', 67: 'О', 68: 'П', 69: 'Р', 70: 'С', 71: 'Т', 72: 'У',
-           73: 'Ф', 74: 'Х', 75: 'Ц', 76: 'Ч', 77: 'Ш', 78: 'Щ', 79: 'Ъ', 80: 'Ы', 81: 'Ь', 82: 'Э', 83: 'Ю', 84: 'Я',
-           85: 'А', 86: 'Б', 87: 'В', 88: 'Г', 89: 'Д', 90: 'Е', 91: 'Ё', 92: 'Ж', 93: 'З', 94: 'И', 95: 'Й', 96: 'К',
-           97: 'Л', 98: 'М', 99: 'Н', 100: 'О', 101: 'П', 102: 'Р', 103: 'С', 104: 'Т', 105: 'У', 106: 'Ф', 107: 'Х',
-           108: 'Ц', 109: 'Ч', 110: 'Ш', 111: 'Щ', 112: 'Ъ', 113: 'Ы', 114: 'Ь', 115: 'Э', 116: 'Ю', 117: 'Я', 118: ' '}
+           25: 'Z', 26: 'А', 27: 'Б', 28: 'В', 29: 'Г', 30: 'Д', 31: 'Е', 32: 'Ё', 33: 'Ж', 34: 'З', 35: 'И', 36: 'Й',
+           37: 'К', 38: 'Л', 39: 'М', 40: 'Н', 41: 'О', 42: 'П', 43: 'Р', 44: 'С', 45: 'Т', 46: 'У', 47: 'Ф', 48: 'Х',
+           49: 'Ц', 50: 'Ч', 51: 'Ш', 52: 'Щ', 53: 'Ъ', 54: 'Ы', 55: 'Ь', 56: 'Э', 57: 'Ю', 58: 'Я', 59: 'А', 60: 'Б',
+           61: 'В', 62: 'Г', 63: 'Д', 64: 'Е', 65: 'Ё', 66: 'Ж', 67: 'З', 68: 'И', 69: 'Й', 70: 'К', 71: 'Л', 72: 'М',
+           73: 'Н', 74: 'О', 75: 'П', 76: 'Р', 77: 'С', 78: 'Т', 79: 'У', 80: 'Ф', 81: 'Х', 82: 'Ц', 83: 'Ч', 84: 'Ш',
+           85: 'Щ', 86: 'Ъ', 87: 'Ы', 88: 'Ь', 89: 'Э', 90: 'Ю', 91: 'Я', 99: " "}
 
 
 def get_key(dictionary: dict, s: str) -> int:
@@ -35,65 +33,99 @@ def append_to_size(word: str, length: int):
 
 
 def сaesar(is_encode: bool) -> str:
-    word = input("Введите слово: ").upper()
-
+    word = input("Введите слово: ")
+    key = input("Введите ключ (цифру): ")
     # кол-во сдвига
-    n = 1 if is_encode else -1
+    n = int(key) if is_encode else -int(key)
     encode = ""
     for char in word:
-        index = get_key(alfavit, char)
-        new_index = index + n
-
-        if new_index == -1:
-            new_index = len(alfavit) - 1
-        elif new_index == len(alfavit):
-            new_index = 0
+        index = get_key(alfavit, char.upper())
+        new_index = (index + n) % len(alfavit)
+        print(index)
+        print(new_index)
 
         encode += alfavit[new_index]
 
     return encode
 
 
-def vigenere() -> str:
+def vigenere(is_encode: bool) -> str:
     # row - key[index] | column - value[index]
-    word = input("Введите слово: ").upper()
-    key = append_to_size(input("Введите ключ шифра: ").upper(), len(word))
+    word = input("Введите слово: ")
+    key = append_to_size(input("Введите ключ шифра: "), len(word))
     arr = get_square_array()
     print(arr)
     new_word = ""
-    for i in range(0, len(word)):
-        row = get_key(alfavit, key[i])
-        column = get_key(alfavit, word[i])
-        new_word += arr[row][column]
+    if is_encode:
+        for i in range(0, len(word)):
+            row = get_key(alfavit, key[i].upper())
+            column = get_key(alfavit, word[i].upper())
+            new_word += arr[row][column]
+            print(f"arr[{row}][{column}] = {arr[row][column]}")
+    else:
+        for i in range(0, len(word)):
+            row = get_key(alfavit, key[i].upper())
+            index = arr[row].index(word[i].upper())
+            new_word += alfavit.get(index)
 
     return new_word
 
 
-def wanderers() -> str:
-    row = int(input("Введите кол-во строк: "))
-    word = input("Введите слово: ").upper()
-    new_word = ""
-    c = 0
-    j = 1
+def wanderers(is_encode: bool) -> str:
+    word = input("Введите слово: ")
+    count_row = int(input("Введите кол-во строк: "))
 
-    word_length = len(word)
-    count_row = word_length // row
-    if word_length % row != 0:
-        count_row = count_row + 1
+    count_column = (len(word)) // count_row if (len(word)) % count_row == 0 else (len(word)) // count_row + 1
 
-    while c < row:
-        sh = c
-        while j <= count_row:
-            if sh <= word_length - 1:
-                new_word += word[sh]
-            else:
-                new_word += " "
-            sh = sh + row
-            j = j + 1
-        c = c + 1
-        j = 1
+    while len(word) < count_column * count_row:
+        print(f"{len(word)} < {count_column} * {count_row}")
+        word += " "
+    print(f"Word: |{word}|")
 
-    return new_word
+    print(f"count row = {count_row} | count column = {count_column}")
+    arr = []
+    if is_encode:
+        index_char = 0
+        for i in range(0, count_row):
+            arr_column = []
+            for j in range(0, count_column):
+                arr_column.append(word[index_char])
+                index_char += 1
+            arr.append(arr_column)
+
+        new_word = ""
+        for i in range(0, count_column):
+            for j in range(0, count_row):
+                new_word += alfavit[get_key(alfavit, arr[j][i].upper())]
+
+        return new_word
+    else:
+        out_text_char = [i for i in range(len(word))]
+        print(out_text_char)
+        index = 0
+        for i in range(0, count_column):
+            for j in range(0, count_row):
+                out_text_char[i + count_column * j] = word[index]
+                index += 1
+
+        new_word = "".join(out_text_char)
+
+        return new_word.rstrip()
+        # index_char = len(word) - 1
+        # for i in range(count_column, 0, -1):
+        #     arr_row = []
+        #     for j in range(count_row, 0, -1):
+        #         arr_row.append(word[index_char])
+        #         index_char -= 1
+        #     arr.append(arr_row)
+        #
+        # print(f"{arr} => len {len(arr)}")
+        # new_word = ""
+        # for i in range(len(arr) - 1, 0, -1):
+        #     for j in range(len(arr[i]) - 1, 0, -1):
+        #         new_word += alfavit[get_key(alfavit, arr[i][j].upper())]
+
+    #return new_word
 
 
 if __name__ == '__main__':
@@ -115,11 +147,15 @@ if __name__ == '__main__':
             is_encode = True if int(question) == 1 else False
             word = сaesar(is_encode)
         elif number == 2:
-            word = vigenere()
+            question = input("1. Шифровать\n2. Расшифровать\n")
+            is_encode = True if int(question) == 1 else False
+            word = vigenere(is_encode)
         elif number == 3:
-            word = wanderers()
+            question = input("1. Шифровать\n2. Расшифровать\n")
+            is_encode = True if int(question) == 1 else False
+            word = wanderers(is_encode)
         else:
             print("Bye bye")
             break
 
-        print(f"Слово: {word}")
+        print(f"Слово: |{word.lower()}|")
